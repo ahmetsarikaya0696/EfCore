9e1d2e201b6680d12e623958771fb113208ba6f3
ahmet sarikaya <ahmetsarikaya0696@gmail.com>
2022-10-27 13:07:40 +0300

    RowVersion

---
+++ Concurrency.Web/Controllers/ProductsController.cs
@@ -0,0 +1,59 @@
+﻿using Concurrency.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Data;
+
+namespace Concurrency.Web.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public ProductsController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Products.ToListAsync());
+        }
+
+        public async Task<IActionResult> Update(int id)
+        {
+            return View(await _context.Products.FindAsync(id));
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Update(Product product)
+        {
+            if (!ModelState.IsValid) return View(product);
+
+            try
+            {
+                _context.Update(product);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            catch (DbUpdateConcurrencyException exception)
+            {
+                var exceptionEntry = exception.Entries.First();
+                var currentProduct = exceptionEntry.Entity as Product;
+                var databaseValues = exceptionEntry.GetDatabaseValues();
+                var clientValues = exceptionEntry.CurrentValues;
+                if (databaseValues == null)
+                {
+                    ModelState.AddModelError(string.Empty, "Bu ürün başka bir kullanıcı tarafından silindi");
+                }
+                else
+                {
+                    var databaseProduct = databaseValues.ToObject() as Product;
+                    ModelState.AddModelError(string.Empty, "Bu ürün başka bir kullanıcı tarafından güncellendi");
+                }
+
+
+                return View(product);
+            }
+        }
+    }
+}
---
+++ Concurrency.Web/Models/AppDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Concurrency.Web.Models
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Product>().Property(p => p.RowVersion).IsRowVersion();
+            modelBuilder.Entity<Product>().Property(p => p.Price).HasPrecision(18, 2);
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
